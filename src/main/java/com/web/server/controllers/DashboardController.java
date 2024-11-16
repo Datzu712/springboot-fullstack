5caf309a589ef1762b8cd296ee03aa7e325c9c88
Datzu712 <devdatzucl@gmail.com>
2024-11-16 15:04:11 -0600

    added spring devtools dependency to pom

--- src/main/java/com/web/server/controllers/DashboardController.java
+++ src/main/java/com/web/server/controllers/DashboardController.java
@@ -1,15 +1,13 @@
 package com.web.server.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
-    @GetMapping("/home")
+public class DashboardController {
+    @GetMapping("/")
     public String get() {
-        System.out.println("controller");
-        return "home";
+        return "dashboard";
     }
 
 }
